758747c736ae95758db1e1d3e4d68cb7ecf92515
Aanand Aman <yamit058@gmail.com>
2024-01-22 02:43:15 -0500

    Remove unneddedVariable

--- src/index.js
+++ src/index.js
@@ -3,12 +3,6 @@
 
 
 
-const unneededVariable = 'This variable is never used';
-
-
-
-
-
 // Read environment variables from an .env file (if present)
 // NOTE: we only need to do this once, here in our app's main entry point.
 require('dotenv').config();
